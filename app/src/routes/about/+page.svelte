671b5ec29349405ccb6da65f25260a03a5a3ebc4
Nikolas Heise <heisenikolas@gmail.com>
2024-12-29 16:59:13 +0100

    adapt the about page

--- app/src/routes/about/+page.svelte
+++ app/src/routes/about/+page.svelte
@@ -26,58 +26,11 @@
 	<div
 		class=" w-[288px] h-[160px] border-[1px] border-gray-400 py-3 px-5 rounded-md font-sans animate-fade bg-black"
 	>
-		<p class="font-bold text-yellow-500">Skills:</p>
+		<p class="font-bold text-yellow-500">Strengths:</p>
 
-		<div class="flex justify-between">
 			<p class="mb-0">Creativity</p>
-			<div class="flex gap-0">
-				<span class="flex gap-0 text-yellow-500">
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-				</span>
-			</div>
-		</div>
-
-		<div class="flex justify-between">
-			<p class="mb-0">Teamwork</p>
-			<div class="flex gap-0">
-				<span class="flex gap-0 text-yellow-500">
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-				</span>
-				<Icon src={Star} size="20" solid mini />
-			</div>
-		</div>
-
-		<div class="flex justify-between">
+			<p class="mb-0">Documentation</p>
 			<p class="mb-0">Analytical Thinking</p>
-			<div class="flex gap-0">
-				<span class="flex gap-0 text-yellow-500">
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-				</span>
-				<Icon src={Star} size="20" solid mini />
-			</div>
-		</div>
-
-		<div class="flex justify-between">
-			<p class="mb-0">Self assessment</p>
-			<div class="flex gap-0">
-				<span class="flex gap-0 text-yellow-500">
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-					<Icon src={Star} size="20" solid mini />
-				</span>
-			</div>
-		</div>
+			<p class="mb-0">Abstraction</p>
 	</div>
 </div>
